0f98530bbc39f0b5fe1a83153575fc2f3317fd1e
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-10-21 23:48:47 +0900

    Update background_img.js
    Add images and make random image

--- JS/background_img.js
+++ JS/background_img.js
@@ -1,6 +1,33 @@
+const img_info = [
+  {
+    painter: "Vincent van Gogh",
+    title: "The starry Night",
+  },
+  {
+    painter: "Georges Seurat",
+    title: "Afternoon on the Island of La Grande Jatte",
+  },
+  {
+    painter: "Édouard Manet",
+    title: "Le Déjeuner sur l’herbe",
+  },
+  {
+    painter: "Pablo Picasso",
+    title: "Guernica",
+  },
+  {
+    painter: "Eugène Delacroix",
+    title: "Liberty Leading the People",
+  },
+  {
+    painter: "Edward Hopper",
+    title: "Nighthawks",
+  },
+];
+
 const body = document.body;
-
 const img = document.createElement("img");
-img.src = "image/0.jpg";
-
+const img_num = Math.floor(Math.random() * img_info.length);
+img.src = `image/${img_num}.jpg`;
+// img.height = 959;
 body.appendChild(img);
