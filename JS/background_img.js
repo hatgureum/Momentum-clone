f7b62d04a3147c28641b96853d578f501a06d8f3
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-10-23 00:14:02 +0900

    Update background_img.js

--- JS/background_img.js
+++ JS/background_img.js
@@ -1,33 +1,5 @@
-const img_info = [
-  {
-    painter: "Vincent van Gogh",
-    title: "The starry Night",
-  },
-  {
-    painter: "Georges Seurat",
-    title: "Afternoon on the Island of La Grande Jatte",
-  },
-  {
-    painter: "Édouard Manet",
-    title: "Le Déjeuner sur l’herbe",
-  },
-  {
-    painter: "Pablo Picasso",
-    title: "Guernica",
-  },
-  {
-    painter: "Eugène Delacroix",
-    title: "Liberty Leading the People",
-  },
-  {
-    painter: "Edward Hopper",
-    title: "Nighthawks",
-  },
-];
-
 const body = document.body;
 const img = document.createElement("img");
-const img_num = Math.floor(Math.random() * img_info.length);
+const img_num = Math.floor(Math.random() * 6);
 img.src = `image/${img_num}.jpg`;
-// img.height = 959;
 body.appendChild(img);
