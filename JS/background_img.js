96f943e3b953044db8a7b5ef970f8e02966457c2
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-10-23 00:41:46 +0900

    Update position : fixed

--- JS/background_img.js
+++ JS/background_img.js
@@ -1,5 +1,5 @@
 const body = document.body;
 const img = document.createElement("img");
-const img_num = Math.floor(Math.random() * 6);
+const img_num = Math.floor(Math.random() * 10);
 img.src = `image/${img_num}.jpg`;
 body.appendChild(img);
