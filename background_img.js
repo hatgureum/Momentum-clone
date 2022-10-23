e5d549596dca6c6ac3ac914d15f2f3711ae2b62d
hatgureum <73741078+hatgureum@users.noreply.github.com>
2022-10-23 22:10:11 +0900

    change path

--- background_img.js
+++
@@ -1,6 +0,0 @@
-const body = document.body;
-const img = document.createElement("img");
-const img_num = Math.floor(Math.random() * 10);
-img.src = `image/${img_num}.jpg`;
-img.classList.add("background-img");
-body.appendChild(img);
